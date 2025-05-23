544c6269d6525b51f1118c341f69e757d8dac820
chanhojeong <chano01794@gmail.com>
2025-05-23 10:53:44 +0900

    Init simple static DevOps portfolio

--- docs/js/checkHandler.js
+++
@@ -1,8 +0,0 @@
-function checkedHandler(checkbox) {
-    if(!checkbox.checked) {
-      document.getElementById("container").style.paddingTop = "4rem";
-    }
-    else {
-      document.getElementById("container").style.paddingTop = "40rem";
-    }
-  }
